41dd4eaf175c311d59dfe124b2107757c56902a4
Ken Collins <ken@metaskills.net>
2011-05-12 09:09:54 -0400

    Fix host/port for capybara-webkit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.resources :demos
+  map.resources :demos,
+                :collection => { :goto_index => :get }
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
--- test/test_helper_integration.rb
+++ test/test_helper_integration.rb
@@ -1,13 +1,40 @@
 require "test_helper"
 require "capybara/rails"
 
-class ActionController::IntegrationTest
+Capybara.default_driver = :webkit
+Capybara.server_port = 50420
+
+module ActionController
   
-  include Capybara::DSL
+  module Integration
+    class Session
+      
+      DEFAULT_HOST = '127.0.0.1'
+      
+      def host
+        DEFAULT_HOST
+      end
+      
+      def reset_with_capybara_webkit!
+        reset_without_capybara_webkit!
+        self.host = DEFAULT_HOST
+      end
+      alias_method_chain :reset!, :capybara_webkit
+      
+      def url_for_with_capybara_webkit(options)
+        options[:host] = host
+        options[:port] = Capybara.server_port
+        url_for_without_capybara_webkit(options)
+      end
+      alias_method_chain :url_for, :capybara_webkit
+      
+    end
+  end
   
-  Capybara.default_driver = :webkit
+  class IntegrationTest
+    include Capybara::DSL
+  end
   
-  
-  
+    
 end
 
