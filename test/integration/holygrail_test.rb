77e0c679ba6522e852b989fe15932efc26566b2b
Ken Collins <ken@metaskills.net>
2011-05-11 21:53:54 -0400

    Add demos controller and functional/integration test.

---
+++ test/integration/holygrail_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper_integration'
+
+class HolygrailTest < ActionController::IntegrationTest
+  
+  test "All things JS related" do
+    visit demos_path
+    assert page.has_selector?('h1#demoIndex'), 'should load the h1 normally'
+    find('h1#demoIndex').click
+    assert !page.has_selector?('h1#demoIndex'), 'should remove h1 after clicking it'
+  end
+  
+  
+end
---
+++ test/test_helper_integration.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+require "capybara/rails"
+
+class ActionController::IntegrationTest
+  
+  include Capybara::DSL
+  
+  Capybara.default_driver = :webkit
+  
+  
+  
+end
+
