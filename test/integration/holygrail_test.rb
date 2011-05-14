e29bdb1a06e742351c144a870ca017fcb7b17d5d
Ken Collins <ken@metaskills.net>
2011-05-13 22:07:22 -0400

    Final notes.

--- test/integration/holygrail_test.rb
+++ test/integration/holygrail_test.rb
@@ -8,11 +8,9 @@ class HolygrailTest < ActionController::IntegrationTest
     assert page.has_selector?('h1#demoIndex'), 'should load the h1 normally'
     find('h1#demoIndex').click
     assert !page.has_selector?('h1#demoIndex'), 'should remove h1 after clicking it'
-    
-    # raise goto_index_demos_url.inspect
-    
     # Redirect to index
     visit goto_index_demos_path
+    assert page.has_selector?('h1#demoIndex')
     assert_equal demos_url, page.current_url
   end
   
--- test/test_helper_integration.rb
+++ test/test_helper_integration.rb
@@ -6,28 +6,24 @@ Capybara.server_port = 50420
 
 module ActionController
   
+  # If you want to be persnikity about host/port matching backend webkit_server.
   module Integration
     class Session
-      
       DEFAULT_HOST = '127.0.0.1'
-      
       def host
         DEFAULT_HOST
       end
-      
       def reset_with_capybara_webkit!
         reset_without_capybara_webkit!
         self.host = DEFAULT_HOST
       end
       alias_method_chain :reset!, :capybara_webkit
-      
       def url_for_with_capybara_webkit(options)
         options[:host] = host
         options[:port] = Capybara.server_port
         url_for_without_capybara_webkit(options)
       end
       alias_method_chain :url_for, :capybara_webkit
-      
     end
   end
   
@@ -35,6 +31,12 @@ module ActionController
     include Capybara::DSL
   end
   
-    
+end
+
+# If you want to use transitional fixtures and force all threads to 1 DB connection.
+class ActiveRecord::Base
+  def self.connection
+    @@shared_connection ||= retrieve_connection
+  end
 end
 
